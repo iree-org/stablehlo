a2727e576ec6f18acc561333dfd70ef49d5f1cdf
Stefan Schuermans <schuermans@roofline.ai>
2026-06-08 16:27:45 +0200

    Adaptions to llvm/llvm-project@917117ceeebf for integrating LLVM into IREE
    Drop getEmptyKey() and getTombstoneKey() for DenseMapInfo-related classes.
    The reason is that LLVM dropped those in:
    1e3dc606df3a [DenseMap] Store occupancy in a packed used-bit array (#201281)
    1f10f1ca8af3 [DenseMap] Replace tombstone deletion with TAOCP 6.4 Algorithm R (#200595)
    
    Signed-off-by: Stefan Schuermans <schuermans@roofline.ai>

--- stablehlo/dialect/StablehloOps.cpp
+++ stablehlo/dialect/StablehloOps.cpp
@@ -3720,14 +3720,6 @@ enum NonSpatialDim : int64_t {
 };
 
 struct DenseMapInfoNonSpatialDim {
-  static inline NonSpatialDim getEmptyKey() {
-    return NonSpatialDim(DenseMapInfo<int64_t>::getEmptyKey());
-  }
-
-  static inline NonSpatialDim getTombstoneKey() {
-    return NonSpatialDim(DenseMapInfo<int64_t>::getTombstoneKey());
-  }
-
   static unsigned getHashValue(const NonSpatialDim& key) {
     return DenseMapInfo<int64_t>::getHashValue(key);
   }
